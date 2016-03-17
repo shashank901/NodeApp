650e4cd2280f4fa4506525ce58e2350739953203
Slurricane <chriscoleman@live.com>
2016-03-16 21:15:38 -0700

    sneaky

---
+++ config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by chris on 2/20/2016.
+ */
+module.exports = {
+
+    //Connection URL to mongoDB
+    database_url : 'mongodb://admin:pw123@apollo.modulusmongo.net:27017/quwon7uX',
+
+    //Auth metadata for MailGun
+    mailgun_auth: {
+        auth: {
+            api_key: 'key-9901bf3d7db4cc2015f053eca3ab3f78',
+            domain: 'sandbox4800aadfa17a40db985824c28c218a14.mailgun.org'
+        }
+    }
+};
